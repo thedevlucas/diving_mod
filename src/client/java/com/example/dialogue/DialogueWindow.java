6b5c8351b23f396412fc9f23b0b971fca7d6bece
guillesara <guillermosaraciniet32@gmail.com>
2024-11-11 15:18:57 -0300

    ui va con el damage

--- src/client/java/com/example/dialogue/DialogueWindow.java
+++ src/client/java/com/example/dialogue/DialogueWindow.java
@@ -14,10 +14,18 @@ public class DialogueWindow implements IDialogueWindow {
     private long lastUpdateTime;
     private final Identifier oxigen_frame = new Identifier(Diving_mod.MOD_ID, "textures/gui/oxigen_tank_ui.png");
 
+    private static int currentDamage = 0;
+    private static int maxDamage = 400;
+
     public DialogueWindow(MinecraftClient client, int duration) {
         this.client = client;
         this.duration = duration;
         this.lastUpdateTime = System.currentTimeMillis();
+    }
+
+    public static void setDamage(int damage, int max) {
+        currentDamage = damage;
+        maxDamage = max;
     }
 
     @Override
@@ -25,15 +33,18 @@ public class DialogueWindow implements IDialogueWindow {
         int screenWidth = client.getWindow().getScaledWidth();
         int screenHeight = client.getWindow().getScaledHeight();
 
-
         int height = 50;
         int width = 20;
-        int x = (screenWidth - width) / 2;
-        int y = screenHeight - height - 60;
+        int x = 10;
+        int y = screenHeight - height - 5;
 
+        int fillHeight = (int)((1 - (double)currentDamage / maxDamage) * height);
+        fillHeight = Math.min(fillHeight + 11, height);
 
-        context.drawTexture(oxigen_frame, 10, screenHeight - height - 5, 0, 0, width, height, width, height);
+        int fillY = y + (height - fillHeight);
 
+        context.fill(x + 4, fillY + 9, x + (width - 3), y + (height - 2), 0x80FF0000);
+        context.drawTexture(oxigen_frame, x, y, 0, 0, width, height, width, height);
     }
 
     @Override
