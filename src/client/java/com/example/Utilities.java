6fd3ee098ee882f3bd82fbe9b3d249c9def03f38
guillesara <107478295+guillesara@users.noreply.github.com>
2024-11-13 14:09:35 -0300

    Update Utilities.java

--- src/client/java/com/example/Utilities.java
+++ src/client/java/com/example/Utilities.java
@@ -8,15 +8,6 @@ import net.minecraft.item.ItemStack;
 public class Utilities {
     public static boolean shouldRender = true;
 
-    public static int rgba(int red, int green, int blue, float alpha) {
-        red = Math.max(0, Math.min(255, red));
-        green = Math.max(0, Math.min(255, green));
-        blue = Math.max(0, Math.min(255, blue));
-
-        int alphaInt = Math.round(Math.max(0, Math.min(1, alpha)) * 255);
-        return (alphaInt << 24) | (red << 16) | (green << 8) | blue;
-    }
-
     public static void addDialogue() {
         synchronized (DialogueManager.windows) {
             for (IDialogueWindow window : DialogueManager.windows) {
